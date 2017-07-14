9e6c0e2d9e50cfebacd70f573696ddecb86c4f59
Scott Pierce <scottpiercew@gmail.com>
2017-07-14 16:10:01 -0400

    def index and detail complete, started def vote

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+#the namespace allows the link in index.html to use Url
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /polls/
     url(r'^$', views.index, name='index'),
+    #Using the parentheses around a pattern “captures” the text matched by that pattern and sends it as an argument to the view function
+    # /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
